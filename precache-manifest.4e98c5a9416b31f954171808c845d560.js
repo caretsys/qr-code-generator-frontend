6291bbdb6c36c2895060c02171d8cab59248c31f
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-12-09 23:06:02 +0545

    Updates

---
+++ precache-manifest.4e98c5a9416b31f954171808c845d560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdcf88b0ddb4eee2823202d277d7220e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a87e24dc76a19c7b5499",
+    "url": "/static/css/2.027e9bb2.chunk.css"
+  },
+  {
+    "revision": "00a098c5997a9563b32f",
+    "url": "/static/css/main.a2bfa338.chunk.css"
+  },
+  {
+    "revision": "a87e24dc76a19c7b5499",
+    "url": "/static/js/2.9115c0c3.chunk.js"
+  },
+  {
+    "revision": "610359362e2f9d5fa420eb531608a3c9",
+    "url": "/static/js/2.9115c0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a098c5997a9563b32f",
+    "url": "/static/js/main.dbfdda21.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
